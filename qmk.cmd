f501b155e02488cce5f2ef32c5647bfb0c244ae3
fl3pp <20628550+fl3pp@users.noreply.github.com>
2022-10-21 20:00:33 +0200

    Fixed qmk.cmd

--- qmk.cmd
+++ qmk.cmd
@@ -3,4 +3,4 @@
 set MSYSTEM=MINGW64
 set MSYS2_PATH_TYPE=inherit
 
-"%QMK_HOME%\usr\bin\bash.exe" -l -i %*
+"%QMK_HOME%\usr\bin\bash.exe" --login -i -c "cd '%cd%'; if [ ! -z '%*' ]; then eval '%*'; else $SHELL; fi; "
